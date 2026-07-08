8353ed5e3f5e6253b482fb473b4ee6389ba350cd
Ruoyu25 <wry15830151231@qq.com>
2026-07-08 18:07:34 +0800

    Update 1768-merge-strings-alternately.py

--- array-string/1768-merge-strings-alternately.py
+++ array-string/1768-merge-strings-alternately.py
@@ -10,3 +10,16 @@ If one word is longer, add the remaining characters.
 
 Time Complexity: O(n + m)
 Space Complexity: O(n + m)
+
+class Solution:
+  def mergeAlternately(self, word1: str, word2: str) -> str:
+      result = []
+      i = 0
+
+      while i < len(word1) or i < len(word2):
+        if i < len(word1):
+            result.append(word1[i])
+        if i < len(word2):
+            result.append(word2[i])
+        i += 1
+      return "".join(result)
